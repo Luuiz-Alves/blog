71fff7d675f35173bcf2e093c0bc50b2e87ecd1f
Luiz Alves <98660064+Luuiz-Alves@users.noreply.github.com>
2023-06-06 21:13:35 -0300

    elseif e else

--- helpers.php
+++ helpers.php
@@ -1,20 +1,29 @@
-<?php 
-    function saudacao(): string
-    {
-        $hora = 2;
-        $saudacao = '';
+<?php
+function saudacao(): string
+{
+    $hora = date('H');
 
-        if($hora >= 0 && $hora <= 5){
-            $saudacao = 'boa madrugrada';
-        }
-        if($hora >= 6 && $hora <= 12){
-            $saudacao = 'bom dia';
-        }
+    if ($hora >= 0 && $hora <= 5) {
+        $saudacao = 'boa madrugrada';
+    } elseif ($hora >= 6 and $hora <= 12) {
+        $saudacao = 'bom dia';
+    } elseif ($hora >= 13 and $hora <= 18) {
+        $saudacao = 'boa tarde';
+    } else {
+        $saudacao = 'boa noite';
+    }
+    return $saudacao;
+}
 
-        return $saudacao;
+function resumirTexto(string $texto, int $limite, string $continue = '...'): string
+{
+
+    $textoLimpo = trim(strip_tags($texto));
+    if(mb_strlen($textoLimpo) <= $limite){
+        return $textoLimpo;
     }
 
-    function resumirTexto(string $texto, int $limite,string $continue = '...'): string
-    {
-        return $texto;
-    }
+    $resumirTexto = mb_substr($textoLimpo, 0, mb_strrpos(mb_substr($textoLimpo, 0, $limite), ''));
+
+    return $resumirTexto.$continue;
+}
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,23 +8,12 @@
 </head>
 <body>
     <?php 
-        //declare(strict_types = 1);
         require_once 'sistema/configuracao.php';
         include_once 'helpers.php';
 
-        $texto = 'texto para resumir';
+        $texto = '<h1>texto</h1> <p>para</p> resumir';
         
-        $string = 'texto';
-        $int = 10;
-        $float = 9.99;
-        $bool = true;
-        $nulo = null;
-
-        var_dump($texto);
-        echo '<hr>';
-        echo saudacao();
-        echo '<hr>';
-        echo resumirTexto($texto, 100, 'continue');
+        echo resumirTexto($texto, 50);
     ?>
 </body>
 </html>
