6d281592aae9b4c763b8dc4138501dbaebb35bd2
Luiz Alves <98660064+Luuiz-Alves@users.noreply.github.com>
2023-06-28 19:13:56 -0300

    .

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
     <?php 
@@ -12,10 +13,16 @@
         include_once 'helpers.php';
         include './sistema/Nucleo/Mensagem.php';
 
-        $msg = new Mensagem();
-        echo $msg->renderizar();
-        echo '<hr>';
-        var_dump($msg);
+        use sistema\Nucleo\Mensagem;
+        use sistema\Nucleo\Mensagem as msg;
+
+        echo (new msg)->alerta('texto de alerta');
+
+        // $msg = new Mensagem();
+        // echo $msg->sucesso('Mensagem de sucesso')
+        //          ->renderizar();
+
+        // echo (new Mensagem())->erro('Mensagem de erro')->renderizar();
     ?>
 </body>
 </html>
--- sistema/Nucleo/Mensagem.php
+++ sistema/Nucleo/Mensagem.php
@@ -1,11 +1,53 @@
 <?php 
 
+namespace sistema\Nucleo;
+
 /**
+ * Classe Mensagem - responsável por exibir as mensagens do sistema.
  * @author Luiz Alves <l.alves158@hotmail.com>
+ * @copyright Copyright (c) 2023, UnSet
  */
 class Mensagem {
     private $texto;
     private $css;
+
+    public function __toString()
+    {
+        return $this->renderizar();
+    }
+
+    /**
+     * Método responsável pelas mensagens de sucesso
+     * @param string $mensagem
+     * @return Mensagem
+     */
+    public function sucesso(string $mensagem): Mensagem 
+    {
+        $this->css = 'alert alert-success';
+        $this->texto = $this->filtrar($mensagem);
+        return $this;
+    }
+
+    public function erro(string $mensagem): Mensagem 
+    {
+        $this->css = 'alert alert-danger';
+        $this->texto = $this->filtrar($mensagem);
+        return $this;
+    }
+
+    public function alerta(string $mensagem): Mensagem 
+    {
+        $this->css = 'alert alert-warning';
+        $this->texto = $this->filtrar($mensagem);
+        return $this;
+    }
+
+    public function informa(string $mensagem): Mensagem 
+    {
+        $this->css = 'alert alert-primary';
+        $this->texto = $this->filtrar($mensagem);
+        return $this;
+    }
 
     /**
      * Método responsavel pela renderização
@@ -13,7 +55,7 @@ class Mensagem {
      */
     public function renderizar(): string
     {
-        return $this->texto = $this->filtrar('<h1>mensagem de teste</h1>');
+        return "<div class='{$this->css}'>{$this->texto}</div>";
     }
 
     private function filtrar(string $mensagem): string
