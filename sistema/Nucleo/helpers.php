054fcd526fb36c652402db4ef1007e7ab47ae469
Luiz Alves <98660064+Luuiz-Alves@users.noreply.github.com>
2023-07-13 21:05:22 -0300

    .

--- sistema/Nucleo/helpers.php
+++ sistema/Nucleo/helpers.php
@@ -90,7 +90,7 @@ public static function dataAtual(): string
  * @param string $url parte da url ex. admin
  * @return string url completa
  */
-public static function url(string $url): string
+public static function url(string $url = null): string
 {
     $servidor = filter_input(INPUT_SERVER, 'SERVER_NAME');
     $ambiente = ($servidor == 'localhost' ? URL_DESENVOLVIMENTO : URL_PRODUCAO);
